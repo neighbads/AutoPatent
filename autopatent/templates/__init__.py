b423bab02fadeb8d7b44f9916de9814eed5b3d23
neighbads <neighbads@gmail.com>
2026-03-24 21:44:32 +0800

    chore: remove out-of-scope templates package init

--- autopatent/templates/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Template rendering utilities for AutoPatent."""
-
-from __future__ import annotations
-
-from .renderer import RenderedDisclosure, render_disclosure
-
-__all__ = ["RenderedDisclosure", "render_disclosure"]
-
